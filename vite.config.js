efdfd6f30f8b44fe55a7e1c3b32ab6398976a9ed
DeepaShriSG <deepashrig15@gmail.com>
2024-11-29 20:12:54 +0530

    vite config file changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    mimeTypes: {
-      'js': 'text/javascript',
-    },
-  }
 })
